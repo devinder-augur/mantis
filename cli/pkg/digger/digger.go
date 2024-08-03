7cd3e6d269650c8887dc43d682e43df8b24a3750
devinder-augur <devinder.prasad@getaugur.ai>
2024-08-04 00:21:20 +0530

    Update digger.go

--- cli/pkg/digger/digger.go
+++ cli/pkg/digger/digger.go
@@ -610,11 +610,12 @@ func RunJob(
 		switch command {
 		case "mantis plan":
 			err := usage.SendUsageRecord(requestedBy, job.EventName, "plan")
+			log.Info("Mantis plan line 613")			
 			if err != nil {
 				log.Printf("Failed to send usage report. %v", err)
 			}
 			_, _, _, plan, planJsonOutput, err := diggerExecutor.Plan()
-
+			log.Info("Mantis plan line 618")
 			if err != nil {
 				msg := fmt.Sprintf("Failed to Run mantis plan command. %v", err)
 				log.Printf(msg)
@@ -635,6 +636,7 @@ func RunJob(
 				}
 				return fmt.Errorf(msg)
 			}
+			log.Info("Mantis Plan allowed check")
 			if !planIsAllowed {
 				msg := fmt.Sprintf("Plan is not allowed")
 				log.Printf(msg)
@@ -644,13 +646,13 @@ func RunJob(
 				}
 				return fmt.Errorf(msg)
 			} else {
-
+				log.Info("Mantis Plan run detais check")
 				runDetails, err = backendApi.ReportProjectRun(repo, job.ProjectName, runStartedAt, time.Now(), "SUCCESS", command, plan)
 				log.Printf("Run Details are  %v",  runDetails)
 				if err != nil {
 					log.Printf("Error reporting Run: %v", err)
 				} else {
-					log.Printf("Sending Rrun Details  %v",  "1")
+					log.Info("Mantis Plan run detais planjsonoutput")
 					ReportRunData(planJsonOutput, runDetails.Id)
 				}
 
